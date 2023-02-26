88546c0423265ed3e1769980a856ce7ecc974501
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-26 20:30:07 -0300

    Char

---
+++ variaveis/VariaveisChar.java
@@ -0,0 +1,16 @@
+package variaveis;
+
+public class VariaveisChar {
+    
+    public static void main(String[] args) {
+
+        //char o = 'o';
+        //char i = 'i';
+
+        char o = 111;
+        char i = 105;
+
+        System.out.println(""+o+i);
+
+    }
+}
