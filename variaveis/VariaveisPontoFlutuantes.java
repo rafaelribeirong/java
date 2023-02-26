173c2ef02cbfbc429c5f545cff76ea22c596ae53
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-26 20:15:12 -0300

    Variável Flutuante

---
+++ variaveis/VariaveisPontoFlutuantes.java
@@ -0,0 +1,16 @@
+package variaveis;
+
+public class VariaveisPontoFlutuantes {
+    
+    public static void main(String[] args) {
+
+        double valorPassagem = 2.90;
+
+        float valorTomate = 3.95f;
+
+        System.out.println("Valor da passagem = " + valorPassagem);
+
+        System.out.println("Valor do tomate = " + valorTomate);
+
+    }
+}
