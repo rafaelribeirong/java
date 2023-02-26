941684111fff8e22af4d1bf7a5148960d19cf7f5
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-26 20:09:48 -0300

    Variáveis Inteiras

---
+++ variaveis/VariaveisInteiras.java
@@ -0,0 +1,19 @@
+package variaveis;
+public class VariaveisInteiras {
+
+    public static void main(String[] args) {
+
+        byte idade01 = 20;
+
+        short idade02 = 21;
+
+        int idade03 = 22;
+
+        long idade04 = 23;
+
+        System.out.println("Valos variável idade01 = " + idade01);
+        System.out.println("Valos variável idade02 = " + idade02);
+        System.out.println("Valos variável idade03 = " + idade03);
+        System.out.println("Valos variável idade04 = " + idade04);
+    }
+}
