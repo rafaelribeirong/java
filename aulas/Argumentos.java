1e60ba98053707aad23ca3f97e30648a07223be6
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-27 12:53:49 -0300

    Tipos de dados.

--- aulas/Argumentos.java
+++
@@ -1,7 +0,0 @@
-class Argumentos {
-    public static void main (String[] args){
-
-        System.out.println("Você Digitou " + args[0]);
-    }
-
-}
--- aulas/Erro01.java
+++
@@ -1,9 +0,0 @@
-class Erro01 {
-    public static void main (String[] args){
-
-        int cont = 0;
-
-        System.out.println("Você Digitou " + cont);
-    }
-
-}
