16f5fbcd6248bdd06c64d805b0285b6215745462
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-27 19:24:27 -0300

    Pedindo um número
    Faça um Programa que peça um número e então mostre a
    mensagem O número informado foi [número].

---
+++ exercicios/NumeroInformado.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class NumeroInformado {
+    
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int nqualquer = scan.nextInt();
+        System.out.println("O número informado foi: " + nqualquer);
+
+        
+
+
+
+
+    }
+}
