b891170096fc3725167671fabc0baa830e308ba8
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-27 23:19:09 -0300

    Salário
    Faça um Programa que pergunte quanto você ganha por hora e o
    número de horas trabalhadas no mês. Calcule e mostre o total do seu
    salário no referido mês.

---
+++ exercicios/SalarioHoras.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class SalarioHoras {
+
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o valor/hora: ");
+        double valorHora = scan.nextDouble();
+
+        System.out.println("Entre com a quantidade de horas trabalhadas no mês: ");
+        double horas = scan.nextDouble();
+
+        double salario = valorHora * horas;
+
+        System.out.println("O salário é de: " + salario);
+    }
+
+}
