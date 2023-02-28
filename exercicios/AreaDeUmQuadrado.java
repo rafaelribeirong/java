b0f1ffa347f1f41de676896eef17ad880f1af38c
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-27 23:19:34 -0300

    Área de um quadrado
    Faça um Programa que calcule a área de um quadrado, em seguida
    mostre o dobro desta área para o usuário.

---
+++ exercicios/AreaDeUmQuadrado.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class AreaDeUmQuadrado {
+    
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o tamanho do lado do quadrado: ");
+        double lado = scan.nextDouble();
+
+        //area = lado * lado
+        double area = Math.pow (lado, 2);
+
+        System.out.println("A área do quadrado é: " + area);
+        System.out.println("O dobro da área do quadrado é: " + (area * 2));
+        
+
+
+    }
+}
