6d1ca937b109cf3783e5f86a4a079260bb14b7b5
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-02-28 22:22:52 -0300

    Temperatura 2
    Faça um Programa que peça a temperatura em graus Celsius,
    transforme e mostre em graus Farenheit.

---
+++ exercicios/TemperaturaGraus2.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class TemperaturaGraus2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em Celcius: ");
+        double c = scan.nextDouble();
+
+
+        double f = (c * 1.8) +32;
+        System.out.println("Convertendo sua temperatura para Celsius será: " + f);
+
+    }
+
+}
+
