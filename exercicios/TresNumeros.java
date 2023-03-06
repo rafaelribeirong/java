eff74f998745d4e6aed07cd3dc89e0178d281d96
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-03-06 16:32:24 -0300

    Números
    Chamando 3 números e imprimindo o maior dentre eles e o menor dentre eles.

---
+++ exercicios/TresNumeros.java
@@ -0,0 +1,30 @@
+package exercicios;
+import java.util.Scanner;
+
+public class TresNumeros {
+    
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Entre com o primeiro número:");
+        int num1 = scan.nextInt();
+
+
+        System.out.println("Entre com o segundo número:");
+        int num2 = scan.nextInt();
+
+
+        System.out.println("Entre com o terceiro número:");
+        int num3 = scan.nextInt();
+
+        if (num1 > num2  && num1 >= num3){
+            System.out.println("Número 1 é maior: " +num1);
+        } else if (num2 > num1  && num2 >= num3){
+            System.out.println("Número 2 é maior: " +num2);
+        } else if (num3 > num1  && num3 >= num2){
+            System.out.println("Número 3 é maior: " +num3);
+
+        }
+    }
+}
