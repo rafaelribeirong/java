c4141aec7fad77253bfeb4b944aa15ed8ca5d9aa
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-03-06 19:08:33 -0300

    Preço

---
+++ PrecoProduto.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrecoProduto {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o primeiro número:");
+        int num1 = scan.nextInt();
+
+        System.out.println("Entre com o segundo número:");
+        int num2 = scan.nextInt();
+
+        System.out.println("Entre com o terceiro número:");
+        int num3 = scan.nextInt();
+
+        
+
+    }
+    
+}
