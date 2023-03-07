e3492e68ec2cc96ab43ca3be46bbe51fdb795d12
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-03-07 00:56:23 -0300

    Salário colaborador

---
+++ exercicios/SalarioColaborador.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class SalarioColaborador {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o salário: ");
+        double salario = scan.nextDouble();
+
+        int percentual = 0;
+        if (salario <= 280){
+            percentual = 20;
+        } else if (salario > 280 && salario < 700){
+            percentual = 15;
+        } else if (salario >= 700 && salario <= 1500){
+            percentual = 10;
+        } else if (salario >= 1500){
+            percentual = 5;
+        }
+
+        double aumento = (salario / 100) * percentual;
+        double salarioAjustado = salario + aumento;
+
+        System.out.println("Salário: " +salario);
+        System.out.println("Percentual: " +percentual);
+        System.out.println("Aumento: " +aumento);
+        System.out.println("Salário Ajustado: " +salarioAjustado);
+    }
+    
+}
