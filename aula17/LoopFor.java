b8e4babb6fea1b20a3f432d4cb8d8a48285de900
Rafael Pereira <91510461+rafaelribeirong@users.noreply.github.com>
2023-03-17 13:10:27 -0300

    Loop for

---
+++ aula17/LoopFor.java
@@ -0,0 +1,29 @@
+package aula17;
+
+public class LoopFor {
+
+    public static void main(String[] args) {
+
+        for (int i=0; i < 5 ; i++){
+            System.out.println("i tem o valor: " + i);
+
+        }
+
+        for (int i=5; i > 0 ; i--){
+            System.out.println("i tem o valor: " + i);
+
+        }
+
+        for (int i=0, j=10; i< j  ; i++, j--){
+            System.out.println("i = " + i + "; j=" +j);
+        }
+
+        int count = 0;
+        for  (;count < 5;){
+            System.out.println("Valor de count: " + count);
+            count +=2;
+
+        }
+    }
+    
+}
